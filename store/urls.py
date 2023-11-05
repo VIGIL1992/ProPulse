7f7b85f519e827b504c182cc2325d5ab6f44d730
VIGIL NATH K K <vigil1992@gmail.com>
2023-11-05 11:42:05 +0530

    5

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    
+    path('category/<slug:category_name>/', views.products, name='products_by_category'),
+    path('search/', views.search, name='search'),
     
     path('about_us/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.db.models import Q
 
 # Create your views here.
 
+# This function will take to home page
 def home(request):
     categories = Category.objects.all()
     products = Product.objects.all()
@@ -17,7 +19,8 @@ def home(request):
     return render(request, 'home.html', context)
 
 
-def products(request):
+# This function will take to product page
+def products(request,category_slug=None):
     products = Product.objects.all()
     categories = Category.objects.all()
     
@@ -26,6 +29,21 @@ def products(request):
         'products'  : products,
     }
     return render(request, 'products.html', context)
+
+
+# This function is for search function and take us to store page
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('product_name').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            
+
+    context = {
+        'products' : products,
+
+    }
+    return render(request, 'products.html',context)
 
 
 def about_us(request):
@@ -38,3 +56,5 @@ def contact_us(request):
     
     
     return render(request, 'contact_us.html'    )
+
+
